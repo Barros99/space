a05a7f2054a40cba16558e96c530837a27bc108c
Barros99 <jeanmeneses@hotmail.com>
2023-01-14 21:50:51 -0300

    finishing

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("<h1>Hi there!</h1>")
+    return render(request, "gallery/index.html")
 
 
+def image(request):
+    return render(request, "gallery/image.html")
