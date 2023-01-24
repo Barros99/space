8f352e16cf41374b1b3559281da563a5e4e7c895
Barros99 <jeanmeneses@hotmail.com>
2023-01-24 11:37:39 -0300

    feat: add login and signup pages

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,6 @@ from gallery.views import image, index, search
 
 urlpatterns = [
     path("", index, name="index"),
-    path("image/<int:foto_id>", image, name="image"),
+    path("image/", image, name="image"),
     path("search", search, name="search"),
 ]
